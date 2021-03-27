983f6ddfc1b072b0622be32289552f37224a0193
JasonPink79 <jaysunrose@aol.com>
2021-03-27 16:25:16 +0000

    Simple Counter w.React

---
+++ src/js/component/counter.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
+
+export function SimpleCounter(props) {
+	return (
+		<div className="counter d-flex flex-row">
+			<FontAwesomeIcon icon={faClock} />
+			<div className="digit">{props.four % 10}</div>
+			<div className="digit">{props.three % 10}</div>
+			<div className="digit">{props.two % 10}</div>
+			<div className="digit">{props.one % 10}</div>
+		</div>
+	);
+}
+
+SimpleCounter.defaultProps = {
+	one: 0,
+	two: 0,
+	three: 0,
+	four: 0
+};
+
+SimpleCounter.propTypes = {
+	one: PropTypes.number,
+	two: PropTypes.number,
+	three: PropTypes.number,
+	four: PropTypes.number
+};
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,23 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import { SimpleCounter } from "./component/counter.js";
+let counter = 0;
+
+setInterval(() => {
+	// first: calculate all digits
+	let one = Math.floor(counter / 1);
+	let two = Math.floor(counter / 10);
+	let three = Math.floor(counter / 100);
+	let four = Math.floor(counter / 1000);
+	// next thing:  render the counter component
+	ReactDOM.render(
+		<SimpleCounter one={one} two={two} three={three} four={four} />,
+		document.querySelector("#app")
+	);
+	// last thing: increment the counter (the idea is that 17 pertains to the component and 18 is about the variable)
+	counter++;
+	console.log(counter);
+}, 1000);
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
